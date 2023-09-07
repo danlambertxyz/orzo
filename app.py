bb4b92cb825dd3468edc6caa3bf0696ac3261544
danlambertxyz <daniel.ll@yahoo.com>
2023-09-07 21:58:47 +0100

    orzo story generator first commit

--- app.py
+++ app.py
@@ -10,26 +10,32 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        name = request.form["name"]
+        emotion = request.form["emotion"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(name, emotion),
             temperature=0.6,
+            max_tokens=500,
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+
+        image_url = generate_image(response.choices[0].text)
+
+        return redirect(url_for("index", result=response.choices[0].text, image_url=image_url))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
+    image_url = request.args.get("image_url")
+    print('result = ',result)
+    return render_template("index.html", result=result, image_url=image_url)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(name, emotion):
+    return f"Write a 3 sentence {emotion} story about a main character called {name.capitalize()}."
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_image(story):
+    response = openai.Image.create(
+        prompt=f'Draw a picture for the following short story- {story}',
+        n=1,
+        size="256x256"
     )
+    return response['data'][0]['url']
